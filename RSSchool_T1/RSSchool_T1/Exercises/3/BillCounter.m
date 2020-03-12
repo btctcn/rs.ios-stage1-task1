8620f98bd94736f38977e1359831eba60352ce11
Andrey Butcitcyn <btctcn@gmail.com>
2020-03-13 02:19:57 +0300

    add: BillCounter implementation

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,25 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    NSMutableArray<NSNumber*> *mutableBill = [NSMutableArray<NSNumber*> arrayWithArray:bill];
+    if(index >= 0 && index < mutableBill.count)
+    {
+        [mutableBill removeObjectAtIndex:index];
+    }
+    
+    NSNumber *actualSumAnna = [mutableBill valueForKeyPath: @"@sum.self"];
+    
+    int sumAnnaInt = sum.integerValue;
+    int actualSumAnnaInt = actualSumAnna.integerValue / 2;
+    
+    if(actualSumAnnaInt == sumAnnaInt)
+    {
+        return @"Bon Appetit";
+    }
+    else
+    {
+        return [NSNumber numberWithInt:(sumAnnaInt - actualSumAnnaInt)].stringValue;
+    }
 }
 
 @end
