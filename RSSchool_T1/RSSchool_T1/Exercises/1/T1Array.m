70881fd5710ce2eb94c9b048ddaaf53c36c72ab8
Andrey Butcitcyn <btctcn@gmail.com>
2020-03-13 01:28:01 +0300

    add: T1Array implementation

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -1,10 +1,45 @@
+#import <Foundation/Foundation.h>
 #import "T1Array.h"
 
 @implementation T1Array
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    if(!sadArray)
+        return @[];
+    
+    if(sadArray.count < 3)
+        return sadArray;
+    
+    NSMutableArray *mutableSadArray = [NSMutableArray arrayWithArray:sadArray];
+    NSMutableArray *indiciesToRemove = [NSMutableArray new];
+    NSUInteger currentArrayLen = sadArray.count;
+    NSUInteger reducedArrayLen = 0;
+    while(reducedArrayLen != currentArrayLen)
+    {
+        currentArrayLen = mutableSadArray.count;
+        for(int i = 1; i < currentArrayLen - 1; i++)
+        {
+            NSNumber *prev = mutableSadArray[i-1];
+            NSNumber *curr = mutableSadArray[i];
+            NSNumber *next = mutableSadArray[i+1];
+            if((prev.intValue + next.intValue) < curr.intValue)
+            {
+                [indiciesToRemove addObject:[NSNumber numberWithInt:i]];
+            }
+        }
+        
+        for(int i = indiciesToRemove.count - 1; i >=0; i--)
+        {
+            [mutableSadArray removeObjectAtIndex:(((NSNumber *)indiciesToRemove[i]).intValue)];
+        }
+        
+        [indiciesToRemove removeAllObjects];
+        
+        reducedArrayLen = mutableSadArray.count;
+    }
+    
+    return mutableSadArray;
 }
 
 @end
